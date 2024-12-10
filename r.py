1e363ce90ceff34248df4401fcac4855beb580fc
Bender <ebtiggemann@inf.ufsm.br>
2024-12-10 00:48:36 -0300

    prints

--- r.py
+++ r.py
@@ -1,11 +1,22 @@
-from scapy.all import sniff, IP, TCP
+from scapy.all import sniff, IP, TCP, Raw, send
 from brainword import *
 
+dropped = 0
+received = 0
+
 def receive_pepekets(packet):
+    global dropped  
+    global received  
+
     if packet.haslayer(TCP):
-        print(f"Pacote recebido de {packet.src}")
+        received += 1
         if packet.haslayer(Raw):
             packet[Raw].load = filter_brainrot_content(packet).encode()
+            dropped += 1
+        
+        print(f"dropados: {dropped}")
+        print(f"receba siuuu melhor do mundo: {received}")
+        print(f"Taxa de drop: {dropped / received if received > 0 else 0:.2f}")
         
         ip_packet = IP(dst="10.1.1.254/24") / packet[TCP] / packet[Raw]
         send(ip_packet)
@@ -18,5 +29,5 @@ def start_router(iface):
         print(f"Erro no start_router: {e}")
 
 if __name__ == "__main__":
-    iface = 'r-eth2'
+    iface = 'r-eth2'  # Replace with the correct network interface
     start_router(iface)
